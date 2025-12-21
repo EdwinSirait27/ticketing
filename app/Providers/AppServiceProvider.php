36360bb5cbaedea2a6d37dea1072548ecbe8065c
IT Development <it.dev@mjm-bali.co.id>
2025-12-21 20:13:15 +0800

    saya lupa pokonya commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,9 +17,9 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void {
-        //  if (env('APP_ENV') !== 'local') {
-        //     URL::forceScheme('https');
-        // }
+         if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
    
 }
