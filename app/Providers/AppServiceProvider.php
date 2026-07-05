b82fd1359cfacb866e6578d64c8cfc43b0880759
Edwin Sirait <drummerboy794@gmail.com>
2026-07-06 03:41:08 +0800

    perbaikan tix dengan attachment s3 dengan trust proxy di app.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,7 +30,7 @@ class AppServiceProvider extends ServiceProvider
             });
         }
         if (env('APP_ENV') !== 'production') {
-            // URL::forceScheme('https');
+            URL::forceScheme('https');
         }
     }
 }
